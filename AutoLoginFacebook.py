05d172fd64ee2f3e8cea9e4214e50dcf653c46c2
Poycodon9x <1621060076@student.humg.edu.vn>
2021-06-01 16:01:49 +0700

    Auto Login Facebook Selenium

---
+++ AutoLoginFacebook.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+driver = webdriver.Chrome()
+driver.set_window_size(700,800)
+
+driver.get('https://wwww.facebook.com/login')
+time.sleep(1)
+
+driver.find_element_by_id('email').send_keys('UID')
+time.sleep(1)
+
+driver.find_element_by_id('pass').send_keys('Pass')
+time.sleep(1)
+
+driver.find_element_by_name('login').click()
+time.sleep(4)
+
+driver.get('https://wwww.facebook.com/sondc98')
+time.sleep(1)
